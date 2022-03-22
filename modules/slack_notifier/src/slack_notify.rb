ab46af56956e644f76d9198045fff2e1dfb279ce
Alex Scarborough <alex@teak.io>
2022-03-22 16:00:08 -0700

    Ensure our actual success text ends up in the undeploy notification.

--- modules/slack_notifier/src/slack_notify.rb
+++ modules/slack_notifier/src/slack_notify.rb
@@ -69,7 +69,7 @@ module SlackNotify
       if out_status == 'complete'
         text = "Completed undeployment of #{deployment_desc}"
         if ENV['UNDEPLOY_TECHNO'] == 'true'
-          { text: text, blocks: [{type: "section", text: { type: "mrkdwn", text: "<https://www.youtube.com/watch?v=Z1TlbLfaJp8>"}}]}
+          { text: text, blocks: [{type: "section", text: { type: "mrkdwn", text: text }}, {type: "section", text: { type: "mrkdwn", text: "<https://www.youtube.com/watch?v=Z1TlbLfaJp8>"}}]}
         else
           { text: text }
         end
