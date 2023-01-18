21e4873fba8b54c2bbcbfa53a3234c290d3fe941
Alex Scarborough <alex@teak.io>
2023-01-18 11:39:53 -0800

    Actually retry auto scaling group creation
    Also document why we're doing our own retry logic.

--- modules/deployomat/src/lib.rb
+++ modules/deployomat/src/lib.rb
@@ -559,12 +559,16 @@ module Deployomat
         # If we got here and never retried, we are in trouble
         # If we got here because a retry succeeded, we're okay.
         raise if retry_count == 0
+      # The most common reason we get here is an eventual consistency issue with target groups.
+      # We see from time to time that AutoScaling isn't able to identify a target group that was
+      # recently created.
       rescue Aws::AutoScaling::Errors::ServiceError => exc
         puts "Error cloning ASG to #{name}. #{exc.class.name} #{exc.message}"
         retry_count += 1
         raise if retry_count >= 3
         puts "Retrying #{retry_count}"
         sleep 2 ** retry_count
+        retry
       end
 
       if template_asg.enabled_metrics && template_asg.enabled_metrics.length > 0
